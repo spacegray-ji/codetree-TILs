706d5ed81bd93ef6b380f0b074e45c7b66c75447
Geonwoo Ji <spacegray.ji@gmail.com>
2024-03-26 16:32:37 +0900

    Title: 알파벳에 따른 평가, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240326/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+cap = input()
+
+if cap == 'S':
+    print('Superior')
+elif cap == 'A':
+    print('Excellent')
+elif cap == 'B':
+    print('Good')
+elif cap == 'C':
+    print('Usually')
+elif cap == 'D':
+    print('Effort')
+else:
+    print('Failure')
