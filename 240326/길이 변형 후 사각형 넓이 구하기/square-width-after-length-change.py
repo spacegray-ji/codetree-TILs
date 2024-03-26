09c45b7a2f8855b1a9a23d655d9bcb377433a889
Geonwoo Ji <spacegray.ji@gmail.com>
2024-03-26 14:11:45 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.py
+++ 240326/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.py
@@ -1,9 +1,9 @@
 import sys
 
-a + 8, b * 3 = map(int, sys.stdin.readline().split())
+a , b = map(int, sys.stdin.readline().split())
 
-# a = a + 8
-# b = b * 3
+a = a + 8
+b = b * 3
 
 print(a)
 print(b)
