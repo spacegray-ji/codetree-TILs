430023ac64e150341c74a902f5405a4fa8ef2135
Geonwoo Ji <spacegray.ji@gmail.com>
2024-09-19 15:55:38 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240919/20대의 평균나이/average-age-of-20.py
+++ 240919/20대의 평균나이/average-age-of-20.py
@@ -1,12 +1,14 @@
 import sys
 
-age = []
+t_age = 0
+cnt = 0
 
 while True:
     buf = int(sys.stdin.readline())
     if buf < 30:
-        age.append(buf)
+        t_age += buf
+        cnt += 1
     else:
         break
 
-print("{:.2f}".format(sum(age)/len(age)))
+print("{:.2f}".format(t_age/cnt))
